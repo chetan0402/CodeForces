c2e1e92019fb1fe6e946c781705225e11b0f7ca9
Chetan0402 <chetansharma67g@gmail.com>
2024-03-03 10:09:00 +0530

    339A done

--- 339A.cpp
+++ 339A.cpp
@@ -1,12 +1,37 @@
 #include<iostream>
+#include<vector>
+#include<string>
 using namespace std;
 
 int main(){
     string line;
     cin>>line;
-    int len=(line.length()+1)/2;
-    int*arr=new int[len];
-    int index=0;
-    for(char)
+    vector<int> list;
+    string temp;
+    for(char c:line){
+        if(c=='+'){
+            list.push_back(stoi(temp));
+            temp="";
+            continue;
+        }
+        temp.append(string(1,c));
+    }
+    list.push_back(stoi(temp));
+    for(int i=0;i<list.size();i++){
+        int smallestIndex=i;
+        for(int ii=i;ii<list.size();ii++){
+            if(list[ii] < list[smallestIndex]){
+                smallestIndex=ii;
+            }
+        }
+        int swap=list[smallestIndex];
+        list[smallestIndex] = list[i];
+        list[i] = swap;
+    }
+    cout << list[0];
+    for(int i=1;i<list.size();i++){
+        cout << "+";
+        cout << list[i];
+    }
     return 0;
 }
