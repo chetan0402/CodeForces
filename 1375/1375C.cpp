82b37fde94c2f255db534ffddfe96105981924fe
Chetan0402 <chetansharma67g@gmail.com>
2025-12-24 09:45:31 +0530

    repovive bootcamp #4 p2

---
+++ 1375/1375C.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+#define int long long
+using namespace std;
+
+void solve(){
+    int n;cin>>n;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++) cin>>arr[i];
+    cout << ((arr[0]<arr[n-1])?"YES":"NO") << endl;
+}
+
+signed main(){
+    ios_base::sync_with_stdio(false);cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--)solve();
+}
