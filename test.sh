58167ac1bc07222a156c3cafd274583ebae71ef5
Chetan0402 <chetansharma67g@gmail.com>
2026-01-07 21:24:22 +0530

    side by side diff bash script

--- test.sh
+++ test.sh
@@ -1,1 +1,15 @@
-python3 test.py $CONTEST $1
+#!/bin/bash
+set -euo pipefail
+
+if [ $# -ne 2 ]; then
+    echo "<contestID> <problemChar>"
+    exit 1
+fi
+
+trap 'rm temp_output.txt' EXIT
+
+g++ "${1}/${1}${2}.cpp" -o "${1}/${1}${2}.exe"
+
+"${1}/${1}${2}.exe" < input.txt &> temp_output.txt
+
+diff -y output.txt temp_output.txt
